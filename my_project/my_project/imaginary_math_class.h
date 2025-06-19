5db8a698ab5535d4cd0ce42c55c7086032b3e481
Gilad Kogan <kogangilad@gmail.com>
2025-06-19 16:20:49 +0300

    test

--- my_project/my_project/imaginary_math_class.h
+++ my_project/my_project/imaginary_math_class.h
@@ -1,1 +1,14 @@
-#pragma once
+#ifndef MY_IMAGINARY_NUMBER_H
+#define MY_IMAGINARY_NUMBER_H
+
+class ImaginaryNumber {
+public:
+
+
+private:
+	int m_imaginary;
+	int m_real;
+};
+
+
+#endif //MY_IMAGINARY_NUMBER_H
