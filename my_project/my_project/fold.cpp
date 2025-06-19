4149dde61c7d1270bf62ba4908235ceb99825743
Gilad Kogan <kogangilad@gmail.com>
2025-06-19 12:02:58 +0300

    Fixed redos

--- my_project/my_project/fold.cpp
+++ my_project/my_project/fold.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 
-constexpr int TABLE_SIZE = 11;
+namespace {
+	constexpr int TABLE_SIZE = 11;
+}
 
-//Prints the multiplication table
-void multiplicationTable();
+
 
 using std::cin;
 using std::cout;
 using std::endl;
 
+//Prints the multiplication table
 void multiplicationTable() {
 	for (int i = 1; i <= TABLE_SIZE; i++) {
 		for (int j = 1; j <= TABLE_SIZE; j++) {
