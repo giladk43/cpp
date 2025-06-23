18af88df34fb02b2f87a3ee81e95dc887bba323f
Gilad Kogan <kogangilad@gmail.com>
2025-06-23 14:53:40 +0300

    Finished one liners

--- my_project/my_project/lineUp.cpp
+++ my_project/my_project/lineUp.cpp
@@ -3,20 +3,24 @@
 #include <vector>
 #include <functional>
 #include <numeric>
+
 #include "lineUp.h"
 
 using namespace std;
 
 int main() {
-	vector<int>v {1, 2, 3, 4, 5, 6, 7, 8, 9, 4, 10 }; // 1
+	
+	vector<int>v {1, 2, 3, 4, 5, 6, 7, 8, 9, 4, 11 }; // 1
 	cout << count_if(v.cbegin(), v.cend(), [](int i) { return i == 4; }) << endl; //2
 	cout << count_if(v.cbegin(), v.cend(), [](int i) { return i > 4; }) << endl; //3
 	_Erase_remove_if(v, [](int n) { return n < 8; }); //4
 	copy(v.cbegin(), v.cend(), ostream_iterator<int>(cout, " ")); //5
-	vector<int>v_diff{}; // 6
-	//minus::operator(v.cbegin(), v.cend());//7
+	cout << endl;
+	vector<int>v_diff (v.size() - 1); // 6
+	transform(v.cbegin() + 1, v.cend(), v.cbegin(), v_diff.begin(), minus<>{}); //7
 	copy(v_diff.cbegin(), v_diff.cend(), ostream_iterator<int>(cout, " ")); //8
 	cout << endl;
-	cout << std::accumulate(v.begin(), v.end(), 0);//9
+	cout << accumulate(v_diff.begin(), v_diff.end(), 0); //9
+	
 
 }
